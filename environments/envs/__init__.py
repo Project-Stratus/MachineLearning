e51b0d7df0a90aaa9f0d6c43b9b495fdc685af4c
ardan-S <166487961+ardan-S@users.noreply.github.com>
2025-06-21 11:18:17 +0200

    Align Balloon1D with 2D features

--- environments/envs/__init__.py
+++ environments/envs/__init__.py
@@ -1,2 +1,2 @@
-from environments.envs.loon import Balloon1DEnv
-from environments.envs.balloon_env import Balloon2DEnv
+from environments.envs.loon_env import Balloon1DEnv, Balloon2DEnv, Balloon3DEnv
+
